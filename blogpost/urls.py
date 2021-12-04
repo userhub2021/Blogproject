62c077769860c55c6f1b534d552ee7b828404079
userhub2021 <taka021gmail.com>
2021-12-04 08:35:39 +0000

    fast commit

---
+++ blogpost/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.views.generic.edit import DeleteView
+from .views import BlogList, BlogDetail, BlogCreate, BlogDelete
+
+urlpatterns = [
+    path('list/', BlogList.as_view(), name='list'),
+    path('detail/<int:pk>', BlogDetail.as_view(), name='detail'),
+    path('create/', BlogCreate.as_view(), name='create'),
+    path('delate/<int:pk>', BlogDelete.as_view(),  name='delete'),
+]
---
+++ blogpost/views.py
@@ -0,0 +1,26 @@
+from re import template
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic.edit import DeleteView
+from .models import BlogModel
+from django.urls import reverse_lazy
+# Create your views here.
+
+class BlogList(ListView):
+    template_name = 'list.html'
+    model = BlogModel
+
+class BlogDetail(DetailView):
+    template_name = 'detail.html'
+    model = BlogModel
+
+class BlogCreate(CreateView): 
+    template_name = 'create.html'
+    model = BlogModel
+    fields = ('title', 'content', 'category')       
+    success_url = reverse_lazy('list')
+
+class BlogDelete(DeleteView):
+    template_name = 'delete.html'
+    model = BlogModel
+    success_url = reverse_lazy('list')    
